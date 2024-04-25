6c1955523fc44d0bab2a2e75b5c4a184a30e04d5
kauraman26 <121130222+kauraman26@users.noreply.github.com>
2024-04-24 23:23:46 -0400

    First commit

---
+++ src/test/java/com/LMS/utility/CommonUtils.java
@@ -0,0 +1,103 @@
+package com.LMS.utility;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.LMS.factory.DriverFactory;
+
+
+public class CommonUtils {
+	
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 10;
+	public static WebDriver driver = DriverFactory.getDriver() ;
+	
+	public static boolean webElement_Click(WebElement element) {
+		try {
+			WebElement ele_toclick = new WebDriverWait(driver, Duration.ofSeconds(10)).
+					until(ExpectedConditions.visibilityOf(element));
+				
+				if(ele_toclick.isEnabled()) {
+					try {
+						ele_toclick.click();
+						return true;
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+						return false;
+					}	
+				}
+				else {
+					throw new Exception("Element is not enabled");
+				}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean webSendKeys(WebElement element, String text) {
+		try {
+			WebElement ele = new WebDriverWait(driver, Duration.ofSeconds(10)).
+					until(ExpectedConditions.visibilityOf(element));
+		
+				if(ele.isEnabled()) {
+					try {
+						ele.clear();
+						ele.sendKeys(text);	
+						return true;
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+						return false;
+					}	
+				}
+				else {
+					throw new Exception("Element is not enabled");
+				}
+			}
+			
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+
+	
+	public String getElementText(WebElement element) {
+		WebElement textElement = new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.visibilityOf(element));
+		return textElement.getText();
+	}
+
+	public boolean verifyElementText(String expectedMsg, WebElement element) {
+		if (getElementText(element).equals(expectedMsg)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	
+}
+
---
+++ src/test/java/com/LMS/utility/ConfigReader.java
@@ -0,0 +1,68 @@
+package com.LMS.utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+	
+	private static Properties prop;
+	private final static String propertyFilePath = "./src/test/resources/config/config.properties";
+
+	/**
+	 * This method is used to load the properties from config.properties file
+	 * @return it returns Properties prop object
+	 */
+	public Properties init_prop() {
+
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream(propertyFilePath);
+			prop.load(ip);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return prop;
+	}
+	
+	public static String getApplicationUrl() {
+		String url = prop.getProperty("url");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("url not specified in the Configuration.properties file.");
+	}
+	
+	public static String getHomePageTitle() {
+		String url = prop.getProperty("homePageTitle");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("homeURL"
+					+ " not specified in the Configuration.properties file.");
+	}
+	
+	public static String getDashboardHeader() {
+		String url = prop.getProperty("dashboardHeader");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException("url not specified in the Configuration.properties file.");
+	}
+	
+	
+	public static String getexcelfilepath() {
+		String excelfilelpath = prop.getProperty("excelfilepath");
+		if (excelfilelpath != null)
+			return excelfilelpath;
+		else
+			throw new RuntimeException("Excel file path not specified in the Configuration.properties file.");
+	}
+	
+	
+}
