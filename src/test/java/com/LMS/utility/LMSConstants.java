b1dc9a3b0b2e9b92bac6d54201ce60de579f79bf
SDETNinjas <159473479+sdetninjas@users.noreply.github.com>
2024-04-30 18:35:03 -0500

    Add files via upload

---
+++ src/test/java/com/LMS/utility/LMSConstants.java
@@ -0,0 +1,17 @@
+package com.LMS.utility;
+
+import java.util.List;
+import java.util.Map;
+
+public class LMSConstants {
+	
+	public static final String EXCELFILEPATH = "excelfilepath";
+	
+	public static String PROGRAM_SHEET_NAME="Program";
+	
+	public static ApplicationData applicationData;
+	
+	public static String LOGIN_SHEET_NAME="Login";
+	
+	
+}
